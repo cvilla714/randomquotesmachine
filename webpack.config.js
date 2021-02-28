0f9e2696df4869069fb004b64616a4010440b098
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-28 16:51:56 -0500

    created the webpack.config.js file and added babel loader and css loader

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
