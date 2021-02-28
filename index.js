0fa371bb0211cb33060255a92bc79e5791720118
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-28 02:06:38 -0500

    added the format for adding the quote and author

--- index.js
+++ index.js
@@ -29,7 +29,25 @@ function generateQuote() {
   // console.log("JS loaded");
   let quoteSize = QuoteBank.length;
   let randomIndexQuote = Math.floor(Math.random() * quoteSize);
+  let twitterLink = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22";
+
+  // formatting quote for twitter
+  let quoteInApiFormat = QuoteBank[randomIndexQuote].quote.replace(/ /g, "%20");
+  // adding the quote
+  twitterLink += quoteInApiFormat;
+
+  // adding author in apit format
+  let authorInApiFormat = QuoteBank[randomIndexQuote].author.replace(/ /g, "%20");
+  // adding the author
+  twitterLink += " - " + authorInApiFormat;
+
+  //  "https://twitter.com/intent/tweet?hashtags=quotes&amp;related=freecodecamp&amp;text=%22If%20you%20can%20dream%20it%2C%20you%20can%20achieve%20it.%22%20Zig%20Ziglar"
+  // <i class="fa fa-twitter"></i>
+
+  ("https://twitter.com/intent/tweet?hashtags=quotes&amp;related=freecodecamp&amp;text=%22Nothing%20is%20impossible%2C%20the%20word%20itself%20says%2C%20%E2%80%9CI%E2%80%99m%20possible!%E2%80%9D%22%20%E2%80%93Audrey%20Hepburn");
+
   console.log(randomIndexQuote);
+  document.querySelector("#tweet-quote").href = twitterLink;
   document.querySelector("#text").innerText = QuoteBank[randomIndexQuote].quote;
   document.querySelector("#author").innerText = QuoteBank[randomIndexQuote].author;
 }
