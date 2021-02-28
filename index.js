135a3366b125eefbc009e3331df4a88638118001
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-28 00:38:26 -0500

    creating the functions for when the page loads and displayign the content

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 window.onload = init;
 function init() {
-  //   console.log("JS loaded");
+  // console.log("JS loaded");
+  // document.querySelector("#text").innerText = "Cosmel";
+  // document.querySelector("#author").innerText = "Author";
+  generateQuote();
+}
+
+const generatorquote = document.querySelector("#new-quote");
+generatorquote.addEventListener("click", generateQuote);
+
+function generateQuote() {
+  console.log("Generate Quote");
+  console.log("JS loaded");
   document.querySelector("#text").innerText = "Cosmel";
+  document.querySelector("#author").innerText = "Author";
 }
