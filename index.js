de4f81998817fc17ee02e521b18535b3286aa978
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-28 01:13:43 -0500

    adding the random generator function to select the quote and author

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const QuoteBank = [
   {
     quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-    autor: "Nelson Mandela",
+    author: "Nelson Mandela",
   },
   {
     quote: "The way to get started is to quit talking and begin doing.",
-    autor: "Walt Disney",
+    author: "Walt Disney",
   },
   {
     quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
-    autor: "Steve Jobs",
+    author: "Steve Jobs",
   },
 ];
 
@@ -25,8 +25,11 @@ const generatorquote = document.querySelector("#new-quote");
 generatorquote.addEventListener("click", generateQuote);
 
 function generateQuote() {
-  console.log("Generate Quote");
-  console.log("JS loaded");
-  document.querySelector("#text").innerText = "Cosmel";
-  document.querySelector("#author").innerText = "Author";
+  // console.log("Generate Quote");
+  // console.log("JS loaded");
+  let quoteSize = QuoteBank.length;
+  let randomIndexQuote = Math.floor(Math.random() * quoteSize);
+  console.log(randomIndexQuote);
+  document.querySelector("#text").innerText = QuoteBank[randomIndexQuote].quote;
+  document.querySelector("#author").innerText = QuoteBank[randomIndexQuote].author;
 }
