b1c447a73201cd73936d9c3f178a7979f37bdbab
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-28 00:49:44 -0500

    added quotes

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const QuoteBank = [
+  {
+    quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    autor: "Nelson Mandela",
+  },
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    autor: "Walt Disney",
+  },
+  {
+    quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+    autor: "Steve Jobs",
+  },
+];
+
 window.onload = init;
 function init() {
   // console.log("JS loaded");
