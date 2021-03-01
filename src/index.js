32a0b5c1d02091c2c415f515a2429113277cced4
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-28 20:21:47 -0500

    imported the QuoteBank from the quote.js file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "./style.css";
-
-const QuoteBank = [
-  {
-    quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-    author: "Nelson Mandela",
-  },
-  {
-    quote: "The way to get started is to quit talking and begin doing.",
-    author: "Walt Disney",
-  },
-  {
-    quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
-    author: "Steve Jobs",
-  },
-];
+import QuoteBank from "./quote";
+// const QuoteBank = [
+// {
+// quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+// author: "Nelson Mandela",
+// },
+// {
+// quote: "The way to get started is to quit talking and begin doing.",
+// author: "Walt Disney",
+// },
+// {
+// quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+// author: "Steve Jobs",
+// },
+// ];
 
 window.onload = init;
 function init() {
